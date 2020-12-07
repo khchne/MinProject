9c5330a33f0750d0cf89f79090b09d51dfcbb18c
khchne <71114702+khchne@users.noreply.github.com>
2020-12-07 12:53:51 +0800

    no message

--- minProject/app/src/main/java/com/example/minproject/MainActivity.java
+++ minProject/app/src/main/java/com/example/minproject/MainActivity.java
@@ -9,7 +9,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+    //todo
+        /*
+        * 1、基本翻译 中——英 （是否可以选择语言
+        * 2、翻译页面展示 （跳转到另外一个界面
+        * 3、翻译界面展示的内容
+        * 4、是否能在主页面的底端添加固定按钮
+        * 5、是否有开启动画
+        *  */
         setContentView(R.layout.activity_main);
     }
 }
